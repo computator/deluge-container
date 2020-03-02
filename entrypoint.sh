e014e2851e067655bba22433e30da92b5b7153d6
Reuben Lifshay <rlifshay@gmail.com>
2020-03-01 22:06:58 -0800

    Create a config file if needed to allow remote access.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,23 @@
 #!/bin/sh
 set -e
 
+CONF="/config/core.conf"
+
+create_conf () {
+	[ -f $CONF ] && return 0
+	cat > $CONF <<-"EOF"
+		{
+			"file": 1,
+			"format": 1
+		}{
+			"allow_remote": true
+		}
+	EOF
+}
+
 if [ "$1" = 'deluged' ]; then
 	shift
+	create_conf
 	exec deluged -d -c /config -L info "$@"
 elif [ "$1" = 'deluge-console' ] || [ "$1" = 'console' ]; then
 	shift
