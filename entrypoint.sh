062c3ae933d095c1cfe968b3d2158a7e4a9ffd8e
Reuben Lifshay <rlifshay@gmail.com>
2020-03-01 21:59:45 -0800

    Add deluge-console to entrypoint.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,11 @@
 set -e
 
 if [ "$1" = 'deluged' ]; then
-	exec deluged -d -c /config -L info
+	shift
+	exec deluged -d -c /config -L info "$@"
+elif [ "$1" = 'deluge-console' ] || [ "$1" = 'console' ]; then
+	shift
+	exec deluge-console -c /config -L info "$@"
 fi
 
 exec "$@"
